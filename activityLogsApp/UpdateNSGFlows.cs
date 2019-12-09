42b8c174cd8fbd4eda182618d9c5673c498b8247
arun542 <arunsingh150693@gmail.com>
2019-12-09 14:17:02 +0530

    updated functions to ignore unavailable locations

--- activityLogsApp/UpdateNSGFlows.cs
+++ activityLogsApp/UpdateNSGFlows.cs
@@ -149,9 +149,10 @@ namespace NwNsgProject
         	
         	Dictionary<string, string> storageloc = new Dictionary<string, string>(); 
         	string[] all_locations = new string[]{"eastasia","southeastasia","centralus","eastus","eastus2","westus","northcentralus","southcentralus","northeurope","westeurope","japanwest","japaneast","brazilsouth","australiaeast","australiasoutheast","southindia","centralindia","westindia","canadacentral","canadaeast","uksouth","ukwest","westcentralus","westus2","koreacentral","koreasouth","francecentral"};
+        	List<string> list_locations = new List<string>(all_locations);
         	foreach (var nsg in nsgresult.value) {
 
-        		if(Array.Exists(all_locations, nsg.location)){
+        		if(list_locations.Contains(nsg.location)){
 			   		string loc_nw = nwList[nsg.location];
 			   		string storageId = "";
 			   		if(storageloc.ContainsKey(nsg.location)){
