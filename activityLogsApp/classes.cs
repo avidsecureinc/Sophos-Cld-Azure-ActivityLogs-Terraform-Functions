dba6a8d17e5d39a17bf2b23b93f23de7851b9b0a
Sachin Kumar <sachin@avidsecure.io>
2020-01-07 16:34:13 +0530

    changed object structure for parsing issue

--- activityLogsApp/classes.cs
+++ activityLogsApp/classes.cs
@@ -276,9 +276,17 @@ class ActivityProperties
     public string eventCategory {get; set;}
     public string eventName {get; set;}
     public string operationId {get; set;}
-    public string eventProperties {get; set;}
+    public EventProperties eventProperties {get; set;}
 }
-
+class EventProperties
+{
+    public string Description {get; set;}
+    public string ResourceName {get; set;}
+    public string title {get; set;}
+    public string type {get; set;}
+    public string details {get; set;}
+    public string subscriptionId {get; set;}
+}
 class ActivityEvidence
 {
     public string role {get; set;}
